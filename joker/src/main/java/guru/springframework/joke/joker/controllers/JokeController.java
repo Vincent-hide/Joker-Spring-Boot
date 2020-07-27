e7568bd041e2ddeed13ef41879b8edc32fb0de84
Vincent-hide <hidehiro.okayama@gmail.com>
2020-07-26 23:17:42 -0400

    template engine added. joke is served

--- joker/src/main/java/guru/springframework/joke/joker/controllers/JokeController.java
+++ joker/src/main/java/guru/springframework/joke/joker/controllers/JokeController.java
@@ -18,7 +18,7 @@ public class JokeController {
 
     @RequestMapping({"/", ""})
     public String showJoke(Model model) {
-        model.addAttribute("joke", jokeService.getJoke());
-        return "chucknorris";
+        model.addAttribute("joke", jokeService.getJoke()); // look for a tag that has attribute of joke
+        return "checknorris"; // the name of html to be render
     }
 }
